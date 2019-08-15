5aefc0d1b8209a5176f83246bd69f285cbe71942
President-Q <41891808+President-Q@users.noreply.github.com>
2019-08-15 22:38:34 +0530

    Initial Commit and Project Import

---
+++ Assets/Client.cs
@@ -0,0 +1,28 @@
+﻿// A C# program for Client 
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    StreamWriter sw;
+    StreamReader sr;
+
+    public void SendToServer()
+    {
+        TcpClient tcpClient = new TcpClient("127.0.0.1", 8532);
+        Stream s = tcpClient.GetStream();
+        sw = new StreamWriter(s);
+        sr = new StreamReader(s);
+        sw.AutoFlush = true;
+        print(sr.ReadLine());
+        sw.WriteLine("Hello from client");
+        s.Close();
+        tcpClient.Close();
+    }
+}
+
+
---
+++ Assets/Server.cs
@@ -0,0 +1,52 @@
+﻿// A C# Program for Server 
+using System;
+using System.IO;
+using System.Collections;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+class Server : MonoBehaviour
+{
+    Thread thrd;
+    TcpListener listener;
+    private void Start()
+    {
+        StartServer();
+
+    }
+    public void StartServer()
+    {
+        IPAddress[] addrs = Dns.GetHostEntry("localhost").AddressList;
+        foreach (IPAddress addr in addrs)
+            print(addr);
+        listener = new TcpListener(addrs[1], 8532);
+        listener.Start();
+        thrd = new Thread(Reciver);
+        thrd.Start();
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            thrd.Abort();
+    }
+    public void Reciver()
+    {
+        while (true)
+        {
+            Socket soc = listener.AcceptSocket();
+            //soc.SetSocketOption(SocketOptionLevel.Socket,
+            //        SocketOptionName.ReceiveTimeout,10000);
+            print("Connected:" + soc.RemoteEndPoint);
+            Stream s = new NetworkStream(soc);
+            StreamReader sr = new StreamReader(s);
+            StreamWriter sw = new StreamWriter(s);
+            sw.AutoFlush = true; // enable automatic flushing
+            sw.WriteLine("Helllo from sever");
+            print(sr.ReadLine());
+        }
+    }
+}
